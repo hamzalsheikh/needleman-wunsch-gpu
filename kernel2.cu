c95ab81f1d0b4215a57102b4176509da9b038169
Remy El Sabeh <remysabeh@gmail.com>
2021-04-20 19:41:48 +0300

    Finalized solution for Milestone #4

--- kernel2.cu
+++ kernel2.cu
@@ -1,71 +1,80 @@
 #include "common.h"
 #include "timer.h"
+#include <cuda_profiler_api.h>
 
-#define COARSE_FACTOR 2
+#define COARSE_FACTOR 2 
 #define BLOCK_DIM 1024
 
 __global__ void nw_upper_left_kernel2(unsigned char* reference, unsigned char* query, int* matrix, unsigned int N, int verticalBlockOffset, int horizontalBlockOffset) {
 
-    __shared__ int leftCol_s[BLOCK_DIM];
-    __shared__ int upperRow_s[BLOCK_DIM];
-    __shared__ int diagonal1_s[BLOCK_DIM];
-    __shared__ int diagonal2_s[BLOCK_DIM];
+    int threadArray[COARSE_FACTOR];
+    const int dim = BLOCK_DIM*COARSE_FACTOR;
+    __shared__ int leftCol_s[dim];
+    __shared__ int upperRow_s[dim];
+    __shared__ int diagonal1_s[dim];
+    __shared__ int diagonal2_s[dim];
     int* ptrDiagonal1_s = diagonal1_s;
     int* ptrDiagonal2_s = diagonal2_s;
 
-    int verticalOffset = verticalBlockOffset*blockDim.x;
-    int horizontalOffset = horizontalBlockOffset*blockDim.x;
+    int verticalOffset = verticalBlockOffset*dim;
+    int horizontalOffset = horizontalBlockOffset*dim;
 
-    int startRow = verticalOffset - blockIdx.x*blockDim.x;
-    int startCol = horizontalOffset + blockIdx.x*blockDim.x;
+    int startRow = verticalOffset - blockIdx.x*dim;
+    int startCol = horizontalOffset + blockIdx.x*dim;
 
+    for (int i = 0; i < COARSE_FACTOR ; i++){
     // Load the row at the top of the triangle into shared memory
-    if (startCol + threadIdx.x < N && startRow != 0) {
-        upperRow_s[threadIdx.x] = matrix[(startRow - 1)*N + startCol + threadIdx.x];
+        if (startCol + threadIdx.x + i*BLOCK_DIM < N && startRow != 0) {
+                upperRow_s[threadIdx.x + i*BLOCK_DIM] = matrix[(startRow - 1)*N + startCol + threadIdx.x + i*BLOCK_DIM];
+        }
     }
-
+    for (int i = 0; i < COARSE_FACTOR ; ++i){
     // Load the column to the left of the triangle into shared memory
-    if (startRow + threadIdx.x < N && startCol != 0) {
-        leftCol_s[threadIdx.x] = matrix[(startRow + threadIdx.x)*N + startCol - 1];
-     }
-
+        if (startRow + threadIdx.x + i*BLOCK_DIM < N && startCol != 0) {
+                leftCol_s[threadIdx.x + i*BLOCK_DIM] = matrix[(startRow + threadIdx.x + i*BLOCK_DIM)*N + startCol - 1];
+        }
+    }
     __syncthreads();
-    for (int i = 0; i < blockDim.x; i++) {
-
+    for (int i = 0; i < blockDim.x*COARSE_FACTOR; i++){
         int diagStartRow = startRow + i;
         int diagStartCol = startCol;
-        int outRow = diagStartRow - threadIdx.x;
-        int outCol = diagStartCol + threadIdx.x;
-        // Arbitrary value just for the code to compile
-        int max = -1;
+        for (int j = 0; j < COARSE_FACTOR; j++){
+                int outRow = diagStartRow - (threadIdx.x*COARSE_FACTOR + j);
+                int outCol = diagStartCol + (threadIdx.x*COARSE_FACTOR + j);
 
-        if (threadIdx.x <= i && outRow < N && outCol < N) {
-            // Get neighbors
-            int top = (outRow == 0)?((outCol + 1)*DELETION):(outRow == startRow ? upperRow_s[threadIdx.x] : ptrDiagonal2_s[threadIdx.x]);
-            int left = (outCol == 0) ? ((outRow + 1)*INSERTION) : (threadIdx.x== 0 ? leftCol_s[i] : (ptrDiagonal2_s[threadIdx.x - 1]));
-            int topleft = (outRow == 0) ? (outCol*DELETION) :
-((outCol == 0) ? (outRow*INSERTION) :
-((threadIdx.x == 0 && i == 0) ? (matrix[(outRow - 1)* N + outCol - 1]) :
-((threadIdx.x == 0) ? (leftCol_s[i-1]) :
-((outRow == startRow) ? (upperRow_s[threadIdx.x - 1]) : ptrDiagonal1_s[threadIdx.x -1]))));
+                if (threadIdx.x*COARSE_FACTOR + j <= i && outRow < N && outCol < N) {
+                        // Get neighbors
+                        int m = threadIdx.x * COARSE_FACTOR + j;
+                        int top = (outRow == 0)?((outCol + 1)*DELETION):(outRow == startRow ? upperRow_s[m] : ptrDiagonal2_s[m]);
+                        int left = (outCol == 0) ? ((outRow + 1)*INSERTION) : (threadIdx.x == 0 && j == 0 ? leftCol_s[i] : (ptrDiagonal2_s[m - 1]));
+                        int topleft = (outRow == 0) ? (outCol*DELETION) :
+                        ((outCol == 0) ? (outRow*INSERTION) :
+                        ((threadIdx.x == 0 && i == 0) ? (matrix[(outRow - 1)* N + outCol - 1]) :
+                        ((threadIdx.x == 0 && j == 0) ? (leftCol_s[i-1]) :
+                        ((outRow == startRow) ? (upperRow_s[m - 1]) : ptrDiagonal1_s[m -1]))));
 
-            // Find scores based on neighbors
-            int insertion = top + INSERTION;
-            int deletion = left + DELETION;
-            int match = topleft + ((query[outRow] == reference[outCol])?MATCH:MISMATCH);
+                        // Find scores based on neighbors
+                        int insertion = top + INSERTION;
+                        int deletion = left + DELETION;
+                        int match = topleft + ((query[outRow] == reference[outCol])?MATCH:MISMATCH);
 
-            // Select best score
-            max = (insertion > deletion)?insertion:deletion;
-            max = (match > max)?match:max;
-            matrix[outRow *N + outCol] = max;
+                        // Select best score
+                        int max = (insertion > deletion)?insertion:deletion;
+                        max = (match > max)?match:max;
+                        matrix[outRow *N + outCol] = max;
+                        threadArray[j] = max;
+                }
         }
         __syncthreads();
 
         // Update the diagonals and swap them
-        if (threadIdx.x <= i && outRow < N && outCol < N) {
-            ptrDiagonal1_s[threadIdx.x] = max;
+        for (int k = 0; k < COARSE_FACTOR; ++k){
+                int outRow = diagStartRow - (threadIdx.x*COARSE_FACTOR + k);
+                int outCol = diagStartCol + (threadIdx.x*COARSE_FACTOR + k);
+                if (threadIdx.x*COARSE_FACTOR + k <= i && outRow < N && outCol < N) {
+                        ptrDiagonal1_s[threadIdx.x*COARSE_FACTOR + k] = threadArray[k];
+                }
         }
-
         __syncthreads();
 
         int* temp = ptrDiagonal1_s;
@@ -78,70 +87,83 @@ __global__ void nw_upper_left_kernel2(unsigned char* reference, unsigned char* q
 
 __global__ void nw_lower_right_kernel2(unsigned char* reference, unsigned char* query, int* matrix, unsigned int N, int verticalBlockOffset, int horizontalBlockOffset) {
 
-    __shared__ int diagonal1_s[BLOCK_DIM];
-    __shared__ int diagonal2_s[BLOCK_DIM];
+    int threadArray[COARSE_FACTOR];
+
+    __shared__ int diagonal1_s[BLOCK_DIM*COARSE_FACTOR];
+    __shared__ int diagonal2_s[BLOCK_DIM*COARSE_FACTOR];
     int* ptrDiagonal1_s = diagonal1_s;
     int* ptrDiagonal2_s = diagonal2_s;
 
-    int verticalOffset = verticalBlockOffset*blockDim.x;
-    int horizontalOffset = horizontalBlockOffset*blockDim.x;
+    int verticalOffset = verticalBlockOffset*blockDim.x*COARSE_FACTOR;
+    int horizontalOffset = horizontalBlockOffset*blockDim.x*COARSE_FACTOR;
 
-    int startRow = verticalOffset + blockDim.x - 1 - blockIdx.x*blockDim.x;
-    int startCol = horizontalOffset + 1 + blockIdx.x*blockDim.x;
+    int startRow = verticalOffset + blockDim.x*COARSE_FACTOR - 1 - blockIdx.x*blockDim.x*COARSE_FACTOR;
+    int startCol = horizontalOffset + 1 + blockIdx.x*blockDim.x*COARSE_FACTOR;
 
     // Load the values that come before the first diagonal into shared memory
-    if ((startRow - threadIdx.x >= 0 && startRow - threadIdx.x < N) && startCol - 1 + threadIdx.x < N) {
-        ptrDiagonal1_s[threadIdx.x] = matrix[(startRow - threadIdx.x)*N + (startCol - 1 + threadIdx.x)];
+    for (int s = 0 ; s < COARSE_FACTOR ; ++s){
+        if ((startRow - (threadIdx.x*COARSE_FACTOR + s) >= 0 && startRow - (threadIdx.x*COARSE_FACTOR + s) < N) && startCol - 1 + threadIdx.x*COARSE_FACTOR + s < N) {
+                ptrDiagonal1_s[threadIdx.x*COARSE_FACTOR + s] = matrix[(startRow - (threadIdx.x*COARSE_FACTOR +s))*N + (startCol - 1 + (threadIdx.x*COARSE_FACTOR +s))];
+        }
     }
-
     __syncthreads();
 
-    for (int i = blockDim.x - 2; i >= 0; i--) {
+    for (int i = blockDim.x*COARSE_FACTOR - 2; i >= 0; i--) {
         int diagStartRow = startRow;
-        int diagStartCol = startCol + (blockDim.x - 2 - i);
-        int outRow = diagStartRow - threadIdx.x;
-        int outCol = diagStartCol + threadIdx.x;
-        // Arbitrary value just for the code to compile
-        int max = -1;
+        int diagStartCol = startCol + (blockDim.x*COARSE_FACTOR - 2 - i);
 
-        if (threadIdx.x <= i && outRow < N && outCol < N) {
+        for (int k = 0 ; k < COARSE_FACTOR ; k++){
+                int outRow = diagStartRow - (threadIdx.x*COARSE_FACTOR + k);
+                int outCol = diagStartCol + (threadIdx.x*COARSE_FACTOR + k);
 
-            // Get neighbors
-            int top = (outRow == 0)?((outCol + 1)*DELETION):(i == blockDim.x - 2 ? ptrDiagonal1_s[threadIdx.x + 1] : ptrDiagonal2_s[threadIdx.x + 1]);
-            int left = (outCol == 0)?((outRow + 1)*INSERTION):((i == blockDim.x - 2) ? ptrDiagonal1_s[threadIdx.x] : ptrDiagonal2_s[threadIdx.x]);
+                if (threadIdx.x*COARSE_FACTOR + k <= i && outRow < N && outCol < N) {
 
-            int topleft = (outRow == 0)?(outCol*DELETION):
-((outCol == 0)?(outRow*INSERTION):
-((i == blockDim.x - 2) ? (matrix[(outRow - 1)*N + (outCol - 1)]):
-(ptrDiagonal1_s[threadIdx.x + 1])));
+                // Get neighbors
+                int m = threadIdx.x * COARSE_FACTOR + k;
+                int top = (outRow == 0)?((outCol + 1)*DELETION):(i == blockDim.x*COARSE_FACTOR - 2 ? ptrDiagonal1_s[m + 1] : ptrDiagonal2_s[m + 1]);
+                int left = (outCol == 0)?((outRow + 1)*INSERTION):((i == blockDim.x*COARSE_FACTOR - 2) ? ptrDiagonal1_s[m] : ptrDiagonal2_s[m]);
 
-            // Find scores based on neighbors
-            int insertion = top + INSERTION;
-            int deletion = left + DELETION;
-            int match = topleft + ((query[outRow] == reference[outCol])?MATCH:MISMATCH);
+                int topleft = (outRow == 0)?(outCol*DELETION):
+                ((outCol == 0)?(outRow*INSERTION):
+                ((i == blockDim.x*COARSE_FACTOR - 2) ? (matrix[(outRow - 1)*N + (outCol - 1)]):
+                (ptrDiagonal1_s[m + 1])));
 
-            // Select best score
-            max = (insertion > deletion)?insertion:deletion;
-            max = (match > max)?match:max;
-            matrix[outRow*N + outCol] = max;
+                // Find scores based on neighbors
+                int insertion = top + INSERTION;
+                int deletion = left + DELETION;
+                int match = topleft + ((query[outRow] == reference[outCol])?MATCH:MISMATCH);
 
+                // Select best score
+                int max = (insertion > deletion)?insertion:deletion;
+                max = (match > max)?match:max;
+                matrix[outRow*N + outCol] = max;
+                threadArray[k] = max;
+                }
         }
         __syncthreads();
 
-        if (i == blockDim.x - 2) {
-            // First diagonal writes directly in diagonal 2
-            if (threadIdx.x <= i && outRow < N && outCol < N) {
-                ptrDiagonal2_s[threadIdx.x] = max;
+
+        if (i == blockDim.x*COARSE_FACTOR - 2) {
+            for (int t = 0; t < COARSE_FACTOR; ++t){
+                int outRow = diagStartRow - (threadIdx.x*COARSE_FACTOR + t);
+                int outCol = diagStartCol + (threadIdx.x*COARSE_FACTOR + t);
+                // First diagonal writes directly in diagonal 2
+                if (threadIdx.x*COARSE_FACTOR + t <= i && outRow < N && outCol < N) {
+                        ptrDiagonal2_s[threadIdx.x*COARSE_FACTOR + t] = threadArray[t];
+                }
             }
         }
         else {
-            // The rest of the diagonals write in diagonal 1 and then 
-            // the pointers are swapped
-            if (threadIdx.x <= i && outRow < N && outCol < N) {
-                ptrDiagonal1_s[threadIdx.x] = max;
-            }
-
-            __syncthreads();
+            for (int t = 0; t < COARSE_FACTOR; ++t){
+                int outRow = diagStartRow - (threadIdx.x*COARSE_FACTOR + t);
+                int outCol = diagStartCol + (threadIdx.x*COARSE_FACTOR + t);
+                // The rest of the diagonals write in diagonal 1 and then
+                // the pointers are swapped
+                if (threadIdx.x*COARSE_FACTOR + t <= i && outRow < N && outCol < N) {
+                        ptrDiagonal1_s[threadIdx.x*COARSE_FACTOR + t] = threadArray[t];
+                }
+         }
+          __syncthreads();
 
             int* temp = ptrDiagonal1_s;
             ptrDiagonal1_s = ptrDiagonal2_s;
@@ -155,7 +177,8 @@ __global__ void nw_lower_right_kernel2(unsigned char* reference, unsigned char* 
 void nw_gpu2(unsigned char* reference_d, unsigned char* query_d, int* matrix_d, unsigned int N) {
 
     const unsigned int numThreadsPerBlock = BLOCK_DIM;
-    const unsigned int numKernelCalls = (N - 1 + numThreadsPerBlock)/numThreadsPerBlock;
+    const unsigned int sizeOfTriangle = numThreadsPerBlock*COARSE_FACTOR;
+    const unsigned int numKernelCalls = (N - 1 + sizeOfTriangle)/sizeOfTriangle;
 
     for (int i = 0; i < numKernelCalls; i++) {
         unsigned int numBlocks = i + 1;
@@ -179,7 +202,7 @@ void nw_gpu2(unsigned char* reference_d, unsigned char* query_d, int* matrix_d, 
         nw_upper_left_kernel2<<< numBlocks, numThreadsPerBlock >>>(reference_d, query_d, matrix_d, N, numKernelCalls - 1, horizontalOffset);
 
         cudaDeviceSynchronize();
-        
+
         // Second call concerns the lower right triangles
         nw_lower_right_kernel2<<< numBlocks, numThreadsPerBlock >>>(reference_d, query_d, matrix_d, N, numKernelCalls - 1, horizontalOffset);
 
